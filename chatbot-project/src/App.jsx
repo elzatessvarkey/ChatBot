1a45547f4ec91248997091be0592e38d6a61a4fe
elzatessvarkey <elzatessvarkey@gmail.com>
2026-01-14 12:31:12 +0100

    Updated the code via proper react setup

---
+++ chatbot-project/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import { ChatInput } from './components/ChatInput'
+import ChatMessages from './components/ChatMessages';
+import './App.css'
+
+    function App() {
+
+      //lifting up the state to share it with both components
+
+      const [chatMessages, setChatMessages] = useState([]);
+
+      return (
+        <div className="app-container">
+          <ChatMessages
+            chatMessages={chatMessages}
+          />
+          <ChatInput
+            chatMessages={chatMessages}
+            setChatMessages={setChatMessages}
+          />
+        </div>
+      );
+    }
+
+export default App
